babeaf50f3d59dc28eed5f17a6e0b45e6a293495
rafael <rafafaine@gmail.com>
2021-04-15 08:42:57 -0300

    Service layer, component registration

--- course/src/main/java/com/rafael/course/repositories/UserRepository.java
+++ course/src/main/java/com/rafael/course/repositories/UserRepository.java
@@ -3,7 +3,7 @@ package com.rafael.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rafael.course.entities.User;
-
+//opcianal o registro da classe como repository pois ela ja herda da classe Jpa Repository;
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	
