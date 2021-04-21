b31cfa683fa07937229749fd4e0218227cfafd45
Rafael <rafafaine@gmail.com>
2021-04-20 21:17:07 -0300

    Payment, one-to-one association

--- course/src/main/java/com/rafael/course/config/TestConfig.java
+++ course/src/main/java/com/rafael/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rafael.course.entities.Category;
 import com.rafael.course.entities.Order;
 import com.rafael.course.entities.OrderItem;
+import com.rafael.course.entities.Payment;
 import com.rafael.course.entities.Product;
 import com.rafael.course.entities.User;
 import com.rafael.course.entities.enuns.OrderStatus;
@@ -88,7 +89,10 @@ public class TestConfig implements CommandLineRunner//utililizado para executar 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 	
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
 		
+		orderRepository.save(o1);
 		
 		
 	}
